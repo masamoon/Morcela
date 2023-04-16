07fab35377acdca89bcddecdc289c8c413115a0a
André Lopes <andrerlopes19@gmail.com>
2023-04-16 23:06:10 +0200

    update design

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 var createError = require('http-errors');
 var express = require('express');
+var favicon = require('serve-favicon');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(favicon(path.join(__dirname, 'public','images','favicon.ico'))); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public','images')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
